696ab75992f611a5cc467637935a82a8c99de66f
Victor Miclovich <vicmiclovich@gmail.com>
2010-10-19 23:23:55 +0300

    utilities for scoop

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+utils contains most of the Machine learning algorithms need to analyze
+training sets of data
+
+We would like to appreciate contributors of the following libraries
+NLTK (largely developed by a wonderful team of NLP researchers http://nltk.org)
+kasiku (inference engine written in Python by mossplix)
+meclassify (classification algorithms: Progammatori senza frontiere)
+"""
